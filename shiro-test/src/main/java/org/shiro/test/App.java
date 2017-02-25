0f0410e2136c1dd27cacb5face879222d77a2ade
remainvitality <remainvitality@163.com>
2017-02-25 22:10:42 +0800

    添加shiro测试

--- shiro-test/src/main/java/org/shiro/test/App.java
+++ shiro-test/src/main/java/org/shiro/test/App.java
@@ -1,13 +1,16 @@
 package org.shiro.test;
 
+import org.apache.log4j.Logger;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	
+	private static final transient Logger log = Logger.getLogger(App.class);
+	
+	public static void main(String[] args) {
+		log.info("App log");
+	}
 }
